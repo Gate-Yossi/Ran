689b9fddfe3a45b0a3f03e64cf46f46dea301549
Yossi <124479603+Gate-Yossi@users.noreply.github.com>
2023-07-09 15:10:32 +0900

    note 2023.07.15 (#4)

--- app/php-fpm/slim_app/app/settings.php
+++ app/php-fpm/slim_app/app/settings.php
@@ -21,6 +21,10 @@ return function (ContainerBuilder $containerBuilder) {
                     'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
                     'level' => Logger::DEBUG,
                 ],
+                'redis' => [
+                    'host' => 'redis',
+                    'port' => 6379,
+                ],
             ]);
         }
     ]);
