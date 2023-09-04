ad5c091e5aac33199fe054126a1d88b948f8924f
Yossi <124479603+Gate-Yossi@users.noreply.github.com>
2023-09-04 21:35:44 +0900

    note 2023.09.28 (#21)

--- app/k6/scripts/http_get.js
+++ app/k6/scripts/http_get.js
@@ -7,6 +7,10 @@ export const options = {
   stages: [
     { target:  5, duration: '30s' }
   ],
+  thresholds: {
+    http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+    http_req_duration: ['p(95)<200'], // 95% of requests should be below 200ms
+  },
 };
 
 export default function () {
